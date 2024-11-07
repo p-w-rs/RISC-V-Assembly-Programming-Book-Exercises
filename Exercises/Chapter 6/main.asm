9c82c76ee50d0f6d6f6c5bfc2b364cfc1f2c44e4
Powers, Joshua <dev@powerslabs.org>
2024-11-07 15:56:19 -0700

    Changing structure)

--- Exercises/Chapter 6/main.asm
+++
@@ -1,7 +0,0 @@
-    li a0, 0  # file descriptor = 0 (stdin)
-    la a1, input_address #  buffer to write the data
-    li a2, 1  # size (reads only 1 byte)
-    li a7, 63 # syscall read (63)
-    ecall
-
-input_address: .skip 0x10  # buffer
